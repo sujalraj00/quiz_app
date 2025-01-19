70cc6d9d6465d9011aaa10cbd6b5ab4496fb53c2
Sujal Raj <sujalraj611@gmail.com>
2025-01-19 22:53:24 +0530

    quiz model updated

--- lib/models/quiz_model.dart
+++ lib/models/quiz_model.dart
@@ -1,20 +1,44 @@
 class QuizQuestion {
-  final String question;
-  final int correctAnswer;
-  final int points;
-  final List<String> options;
+  final int id;
+  final String description;
+  final List<QuizOption> options;
+  final String detailedSolution;
 
-  QuizQuestion(
-      {required this.question,
-      required this.correctAnswer,
-      required this.points,
-      required this.options});
+  QuizQuestion({
+    required this.id,
+    required this.description,
+    required this.options,
+    required this.detailedSolution,
+  });
 
   factory QuizQuestion.fromJson(Map<String, dynamic> json) {
     return QuizQuestion(
-        question: json['question'],
-        correctAnswer: json['correctAnswer'],
-        options: List<String>.from(json['options']),
-        points: json['points'] ?? 10);
+      id: json['id'] as int,
+      description: json['description'] as String,
+      options: (json['options'] as List<dynamic>)
+          .map((option) => QuizOption.fromJson(option))
+          .toList(),
+      detailedSolution: json['detailed_solution'] as String? ?? '',
+    );
+  }
+}
+
+class QuizOption {
+  final int id;
+  final String description;
+  final bool isCorrect;
+
+  QuizOption({
+    required this.id,
+    required this.description,
+    required this.isCorrect,
+  });
+
+  factory QuizOption.fromJson(Map<String, dynamic> json) {
+    return QuizOption(
+      id: json['id'] as int,
+      description: json['description'] as String,
+      isCorrect: json['is_correct'] as bool,
+    );
   }
 }
